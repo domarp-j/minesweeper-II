6b3f2197da15a2818a33a5f2e5799de14c2fbcbb
Pramod Jacob <psjacob68@gmail.com>
2020-06-11 11:10:55 -0400

    Fetch board data on client from server

--- client/App.js
+++ client/App.js
@@ -1,14 +1,49 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+
 import "./app.css";
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1 id="title">... xxx XXX Minesweeper XXX xxx ...</h1>
-        <div id="board">
-        </div>
+const App = () => {
+  const WIDTH = 16;
+  const HEIGHT = 16;
+
+  const [board, setBoard] = useState('');
+  const [hasError, setErrorState] = useState(false);
+
+  // Fetch minesweeper (MS) board data from server.
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+
+    // Build API route.
+    let route = '/api';
+    if (params.get('layoutIndex')) {
+      route += `?layoutIndex=${params.get('layoutIndex')}`;
+    }
+
+    // Fetch board from API.
+    fetch(new Request(route)).then((res) => {
+      const result = res.json();
+
+      if (!res.ok) {
+        setErrorState(true);
+      }
+
+      return result;
+    }).then((data) => {
+      setBoard(data.board);
+    });
+  });
+
+  return (
+    <div>
+      <h1 id="title">... xxx XXX Minesweeper XXX xxx ...</h1>
+      <div id="message">
+        {hasError && "Something went wrong when trying to build your board."}
       </div>
-    );
-  }
-}
+      <div id="board">
+        {board}
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,38 @@
 const express = require('express');
 
+const puzzles = require('../server/data/puzzles');
+
 const app = express();
 const SERVER_PORT = 8080;
 
 app.use(express.static('dist'));
 app.use(express.static('public'));
 
-// add server code here
+/**
+ * GET a Minesweeper (MS) board.
+ */
+app.get('/api', (req, res) => {
+  // Let the client handle custom MS boards.
+  if (req.query.board) return res.status(204).end();
+
+  // Send MS board based on layoutIndex.
+  if (req.query.layoutIndex) {
+    if (puzzles[req.query.layoutIndex]) {
+      return res.status(200).json({
+        board: puzzles[req.query.layoutIndex]
+      });
+    }
+
+    return res.status(404).json({
+      errorMessage: 'Puzzle not found at specified index.'
+    });
+  }
+
+  // Send random MS board.
+  const randomIndex = Math.floor(Math.random() * puzzles.length);
+  return res.status(200).json({
+    board: puzzles[randomIndex]
+  });
+});
 
 app.listen(SERVER_PORT, () => console.log(`Node server listening on port ${SERVER_PORT}!`));

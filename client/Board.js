8a38427076ab03bd1dc5ad3ec08110f8a0de6815
Pramod Jacob <psjacob68@gmail.com>
2020-06-12 10:14:01 -0400

    Show surrounding mine count on cell click

--- client/Board.js
+++ client/Board.js
@@ -1,6 +1,51 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from 'prop-types';
 
+const MINE = 'x';
+
+const WIDTH = 16;
+const HEIGHT = 16;
+
+const surroundingMineCount = ({ mat, row, col }) => {
+  let count = 0;
+
+  // Check row above, if possible.
+  if (row > 0) {
+    if (col > 0 && mat[row - 1][col - 1].value === MINE) {
+      count += 1;
+    }
+    if (mat[row - 1][col].value === MINE) {
+      count += 1;
+    }
+    if (col < WIDTH - 1 && mat[row - 1][col + 1].value === MINE) {
+      count += 1;
+    }
+  }
+
+  // Check row below, if possible.
+  if (row < HEIGHT - 1) {
+    if (col > 0 && mat[row + 1][col - 1].value === MINE) {
+      count += 1;
+    }
+    if (mat[row + 1][col].value === MINE) {
+      count += 1;
+    }
+    if (col < WIDTH - 1 && mat[row + 1][col + 1].value === MINE) {
+      count += 1;
+    }
+  }
+
+  // Check left and right cells, if possible.
+  if (col > 0 && mat[row][col - 1].value === MINE) {
+    count += 1;
+  }
+  if (col < WIDTH - 1 && mat[row][col + 1].value === MINE) {
+    count += 1;
+  }
+
+  return count;
+};
+
 const Board = ({ template }) => {
   const [matrix, setMatrix] = useState([]);
 
@@ -9,7 +54,7 @@ const Board = ({ template }) => {
     const mat = [[]];
 
     for (let i = 0; i < template.length; i += 1) {
-      if (mat[mat.length - 1].length === 16) {
+      if (mat[mat.length - 1].length === WIDTH) {
         mat.push([]);
       }
       mat[mat.length - 1].push({
@@ -21,15 +66,13 @@ const Board = ({ template }) => {
     setMatrix(mat);
   }, [template]);
 
-  const handleCellClick = (row, col) => {
+  const revealCell = (row, col) => {
     const mat = JSON.parse(JSON.stringify(matrix));
 
     mat[row][col] = {
       visited: true,
-      value: mat[row][col].value
+      value: mat[row][col].value === MINE ? MINE : surroundingMineCount({ mat, row, col })
     };
-
-    console.log(row, col, mat[row][col]);
 
     setMatrix(mat);
   };
@@ -37,9 +80,9 @@ const Board = ({ template }) => {
   return (
     <div id="board">
       {matrix.map((row, i) => (
-        <div key={i}>
+        <div className="row" key={i}>
           {row.map((cell, j) => (
-            <button type="button" key={`${i}-${j}`} onClick={() => handleCellClick(i, j)}>
+            <button className="cell" type="button" key={`${i}-${j}`} onClick={() => revealCell(i, j)}>
               {cell.visited && cell.value}
             </button>
           ))}
